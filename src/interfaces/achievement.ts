2bacebf4cfcf636b36b88e53a4cd66376584385b
Nahuel Cisa <nahuelcisa@hotmail.com>
2024-01-20 14:42:17 -0300

    creacion de interfaces

---
+++ src/interfaces/achievement.ts
@@ -0,0 +1,5 @@
+export interface Achievement {
+    appname: string;
+    achieved: number;
+    unlocktime: number;
+}
---
+++ src/interfaces/game.ts
@@ -0,0 +1,7 @@
+export interface Game {
+    name: string;
+    playtime_forever: number;
+    appid: number;
+    img_icon_url: string;
+    img_logo_url: string;
+}
